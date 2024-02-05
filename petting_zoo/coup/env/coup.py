202b4e07b415cba78684917067743fee20688d8e
zapper-95 <jbtl68@durham.ac.uk>
2024-02-05 21:17:11 +0000

    Removed RPS rewards

--- petting_zoo/coup/env/coup.py
+++ petting_zoo/coup/env/coup.py
@@ -28,18 +28,6 @@ ACTIONS = [
     "coup"
 ]
 NUM_ITERS = 100
-REWARD_MAP = {
-    (ROCK, ROCK): (0, 0),
-    (ROCK, PAPER): (-1, 1),
-    (ROCK, SCISSORS): (1, -1),
-    (PAPER, ROCK): (1, -1),
-    (PAPER, PAPER): (0, 0),
-    (PAPER, SCISSORS): (-1, 1),
-    (SCISSORS, ROCK): (-1, 1),
-    (SCISSORS, PAPER): (1, -1),
-    (SCISSORS, SCISSORS): (0, 0),
-}
-
 
 
 def env(render_mode=None):
