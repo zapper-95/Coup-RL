abda706046180619aca2478592c38ab0f493fbfa
zapper-95 <jbtl68@durham.ac.uk>
2024-02-06 12:56:50 +0000

    Added action masking

--- petting_zoo/coup/env/run.py
+++ petting_zoo/coup/env/run.py
@@ -1,6 +1,8 @@
 import coup
+import numpy as np
 
-
+action_mask = np.array([1, 1, 1, 1, 1, 1, 1, 0, 0], dtype=np.int8)
+challenge_mask = np.array([0, 0, 0, 0, 0, 0, 0, 1, 1], dtype=np.int8)
 
 env = coup.env(render_mode="human")
 env.reset(seed=42)
@@ -21,15 +23,13 @@ for agent in env.agent_iter():
         env.get_next_agent()
 
     else:
-        fst_action = env.action_space(agent).sample()
-
+        fst_action = env.action_space(agent).sample(action_mask)
         actions.append(fst_action)
         env.update_state({agent:env.get_action_string(fst_action)})
-
+        print(env.get_action_string(fst_action))
 
         scd_agent = env.get_next_agent()
-        scd_action = env.action_space(scd_agent).sample()
-
+        scd_action = env.action_space(scd_agent).sample(challenge_mask)
 
         if env.get_action_string(scd_action) == "challenge":
             actions.append(scd_action) 
@@ -41,13 +41,34 @@ for agent in env.agent_iter():
 
             env.update_state({scd_agent:"counteract"})
 
-            trd_action = env.action_space(agent).sample()
+            trd_action = env.action_space(agent).sample(action_mask)
 
             if env.get_action_string(trd_action) == "challenge":
                 actions.append(trd_action)
                 env.update_state({agent:"challenge"})
 
-        
+
+
+        if len(actions) == 2:
+            if env.get_action_string(actions[1]) == "challenge":
+                if env.action_legal(agent, fst_action):
+                    env.loose_card(scd_agent)
+                else:
+                    env.loose_card(agent)
+            else:
+                if not env.can_counteract(fst_action):
+                    actions.pop()
+            
+        elif len(actions) == 3:
+            if env.counteraction_legal(fst_action, scd_agent):
+                env.loose_card(agent)
+            else:
+                env.loose_card(scd_agent)
+
+
+
+
+
     # reset to the first agent
     env.get_next_agent()
     # iterate through the actions and apply them
