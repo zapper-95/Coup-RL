bf310b5c22713f78cbe4954bff4d59f2486435ab
zapper-95 <jbtl68@durham.ac.uk>
2024-02-14 15:29:48 +0000

    updated to run for coup v1

--- Coup/coup/run.py
+++ Coup/coup/run.py
@@ -1,15 +1,15 @@
-import coup_v0
+import coup_v1
 import numpy as np
 
 
-env = coup_v0.env(render_mode="human")
+env = coup_v1.env(render_mode="human")
 env.reset(seed=42)
 
 for agent in env.agent_iter():
 
     observation, reward, termination, truncation, info = env.last()
     action_mask = observation["action_mask"]
-
+    print(reward)
     if reward != 0:
         print(f"Player 1 reward: {env.rewards['player_1']}")
         print(f"Player 2 reward: {env.rewards['player_2']}")
