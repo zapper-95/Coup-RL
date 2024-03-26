9d51d08e68c45f117c5e6f2dc8ade954680b93b5
zapper-95 <jbtl68@durham.ac.uk>
2024-03-26 17:43:17 +0000

    initial evaluation code

--- Coup/coup/train_ppo.py
+++ Coup/coup/train_ppo.py
@@ -3,6 +3,7 @@ Code to train a PPO agent on the Coup environment using Ray RLlib.
 
 This code is based on the action masking example from the RLlib repository:
 https://github.com/ray-project/ray/blob/master/rllib/examples/action_masking.py#L52
+https://github.com/ray-project/ray/blob/master/rllib/examples/custom_eval.py
 
 """
 
@@ -28,9 +29,51 @@ from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
 from ray.rllib.utils.framework import try_import_torch
 from ray.rllib.models import ModelCatalog
 from ray.rllib.utils.torch_utils import FLOAT_MAX
-
+from ray.rllib.evaluation.metrics import collect_episodes, summarize_episodes
+from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.rllib.evaluation.worker_set import WorkerSet
+from ray.rllib.examples.policy.random_policy import RandomPolicy
 
 torch, nn = try_import_torch()
+
+
+
+def custom_eval_function(algorithm, eval_workers:WorkerSet):
+    """Example of a custom evaluation function.
+
+    Args:
+        algorithm: Algorithm class to evaluate.
+        eval_workers: Evaluation WorkerSet.
+
+    Returns:
+        metrics: Evaluation metrics dict.
+    """
+    print(type(eval_workers))
+    print(dir(eval_workers))
+    # eval_workers.foreach_worker(
+
+    #     func = lambda w: w.foreach_policy(
+    #         lambda 
+    #     )
+
+    #     # func=lambda w: w.foreach_env(
+    #     #     lambda env: env.set_corridor_length(4 if w.worker_index == 1 else 7)
+    #     # )
+    # )
+
+    # Run evaluation episodes.
+    for i in range(100):
+        print("Custom evaluation round", i)
+        # Running one episode per worker.
+        eval_workers.foreach_worker(func=lambda w: w.sample(), local_worker=False)
+
+    # Collect and summarize episodes into a metrics dict.
+    episodes = collect_episodes(workers=eval_workers, timeout_seconds=99999)
+    metrics = summarize_episodes(episodes)
+    print(metrics)
+    return metrics
+
+
 class ActionMaskModel(TorchModelV2, nn.Module):
     """Custom PyTorch model to handle action masking, and processing multi-discrete observations."""
 
@@ -72,7 +115,7 @@ def env_creator(render=None):
 
 if __name__ == "__main__":
 
-    
+    eval_fn = custom_eval_function
     ModelCatalog.register_custom_model("am_model", ActionMaskModel)
 
     register_env("Coup", lambda config: PettingZooEnv(env_creator()))
@@ -88,6 +131,7 @@ if __name__ == "__main__":
             policies={
                 "player_1": (None, obs_space, act_space, {}),
                 "player_2": (None, obs_space, act_space, {}),
+                "random": (RandomPolicy, obs_space, act_space, {}),
             },
             policy_mapping_fn=(lambda agent_id, *args, **kwargs: agent_id),
         )
@@ -121,20 +165,34 @@ if __name__ == "__main__":
             # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
             num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0"))
         )
-        #.rl_module(_enable_rl_module_api=False)
+        .evaluation(
+            evaluation_num_workers=2,
+            # Enable evaluation, once per training iteration.
+            evaluation_interval=1,
+            evaluation_duration=100,
+            evaluation_config={
+                "multiagent": {
+                    "policy_mapping_fn": (lambda agent_id, *args, **kwargs: "player_1" if agent_id == "player_1" else "random")
+                }
+            },       
+            custom_evaluation_function=eval_fn
+        )
 
     )
-    ray.init()
+    ray.init(ignore_reinit_error=True)
 
     os.makedirs("ray_results", exist_ok=True)
 
     tune.run(
         "PPO",
         name="PPO",
-        stop={"training_iteration": 1},
+        stop={"training_iteration": 10},
         checkpoint_config= CheckpointConfig(checkpoint_at_end=True),
         config=config.to_dict(),
         storage_path= os.path.normpath(os.path.abspath("./ray_results")),
     )
 
     print("Finished training.")
+
+
+    ray.shutdown()
